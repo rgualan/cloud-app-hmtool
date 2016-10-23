1fbb7b2a7e559cb0af2a659d2afa67c0c6ddd32e
Jing Wang <junewang.cz@gmail.com>
2016-10-23 19:49:52 +0100

    add JingHandler
    try to add a handler

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -20,6 +20,7 @@ import tornado.options
 import tornado.web
 import tornado.wsgi
 
+import wsgiref.simple_server
 from tornado.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
