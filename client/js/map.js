05adb4b17a038fab7099e33839b520065f53d0d0
Jing Wang <junewang.cz@gmail.com>
2017-01-06 19:22:22 +0000

    [Mod][client] Jshint revision corrections

--- client/js/map.js
+++ client/js/map.js
@@ -1,22 +1,16 @@
 var width = 700,
     height = 700,
     active = d3.select(null);
-
-
 var projection = d3.geo.mercator()
     .scale((width + 1) / 2 / Math.PI)
     .translate([width / 2, height / 2])
-    .precision(.1);
-
+    .precision(0.1);
 var path = d3.geo.path()
     .projection(projection);
-
 var graticule = d3.geo.graticule();
-
 var svg = d3.select("#canvas-svg").append("svg")
     .attr("width", width)
     .attr("height", height);
-
 svg.append("path")
     .datum(graticule)
     .attr("class", "graticule")
@@ -25,14 +19,12 @@ svg.append("path")
 
 d3.json("/json/world-topo-min.json", function (error, world) {
     var countries = topojson.feature(world, world.objects.countries).features;
-
     svg.append("path")
         .datum(graticule)
         .attr("class", "choropleth")
         .attr("d", path);
 
     var g = svg.append("g");
-
     g.append("path")
         .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
         .attr("class", "equator")
@@ -50,31 +42,19 @@ d3.json("/json/world-topo-min.json", function (error, world) {
             else
                 lat_lng[1].push([data_get[i].geometry.coordinates[1], data_get[i].geometry.coordinates[0]]);
         }
-        console.log(lat_lng);
         var country = g.selectAll(".country").data(countries);
-
         country.enter().insert("path")
             .attr("class", "land")
             .attr("d", path)
             .attr("id", function (d, i) {
-                return d.id;
-            })
+                return d.id;})
             .attr("title", function (d) {
-                return d.properties.name;
-            })
-
-
+                return d.properties.name;})
             .on('mouseover', function (d) {
-                d3.select("#country").html("<h4>" + d.properties.name + "</h4>");
-
-            })
+                d3.select("#country").html("<h4>" + d.properties.name + "</h4>");})
             .on('click', function (d) {
-
                 d3.select("#bar_country_name").html(d.properties.name);
-                draw(lat_lng, d.properties.name);
-
-            })
-
+                draw(lat_lng, d.properties.name);})
             .on("mouseout", function (d) {
                 d3.select("#country").html("<h4>World</h4>");
             });
@@ -91,14 +71,11 @@ d3.json("/json/world-topo-min.json", function (error, world) {
 
         svg.attr("height", height * 2.2 / 3);
 
-
         var sen = getseperate(data.features);
         console.log(sen);
         var word = ["bad", "happy"];
         var col = ["#F084B3", "#91E4D5"];
-
-
-        for (var i = 0; i < word.length; i++) {
+        for (i = 0; i < word.length; i++) {
             g.selectAll(".circle_" + word[i])
                 .data(sen[word[i]])
                 .enter()
@@ -117,7 +94,7 @@ d3.json("/json/world-topo-min.json", function (error, world) {
                         fill: col[i],
                         r: 1.5 * 2
                     });
-                    d3.select("#tooltip").transition().duration(200).style("opacity", .9);
+                    d3.select("#tooltip").transition().duration(200).style("opacity", 0.9);
                     d3.select("#tooltip").html("user_id:" + d.user_id + "<br>point:" + d.point)
                         .style("left", (d3.event.pageX) + "px")
                         .style("top", (d3.event.pageY - 28) + "px");
@@ -136,7 +113,7 @@ d3.json("/json/world-topo-min.json", function (error, world) {
                 g.attr("transform", "translate(" +
                     d3.event.translate.join(",") + ")scale(" + d3.event.scale + ")");
                 for (var i = 0; i < word.length; i++) {
-                    var a = g.selectAll("#circle_" + word[i]).attr("d", path.projection(projection));
+                    g.selectAll("#circle_" + word[i]).attr("d", path.projection(projection));
                 }
 
                 g.selectAll("path")
@@ -144,23 +121,20 @@ d3.json("/json/world-topo-min.json", function (error, world) {
 
             });
 
-        function button1() {
-
-        }
-
         d3.select("#back").on("click", function () {
             map_to_bar([sen.bad.length, sen.happy.length]);
             d3.select("#bar_country_name").html("World");
-        })
+        });
         svg.call(zoom);
         map_to_bar([sen.bad.length, sen.happy.length]);
 
 
     });
 });
+//seperate data into two part
 function getseperate(data) {
-    sen = {bad: [], happy: []};
-    for (d in data) {
+    var sen = {bad: [], happy: []};
+    for (var d in data) {
         if (Number(data[d].point) < 0) {
             sen.bad.push(data[d]);
         }
@@ -176,9 +150,7 @@ function getseperate(data) {
 
 function draw(lat_lng, country) {
     d3.csv('js/codegrid/country_code.csv', function (data) {
-        var country_name_code = [];
         var code_select;
-        var n = 0;
         for (var i = 0; i < data.length; i++) {
             if (data[i].Name === country) {
                 code_select = data[i].Code.toLowerCase();
@@ -195,15 +167,15 @@ function draw(lat_lng, country) {
                         count_bad++;
                     }
                 }
-                for (var i = 0; i < code1.length; i++) {
+                for (i = 0; i < code1.length; i++) {
                     if (code1[i] === code_select) {
                         count_happy++;
                     }
                 }
                 map_to_bar([count_bad, count_happy]);
             });
-        })
-    })
+        });
+    });
 }
 
 
