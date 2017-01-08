0577fe7ba6d5e13d55881c68fd3a551c2f171dc4
christina Dilopoulou <cd3n15@ecs.soton.ac.uk>
2017-01-08 14:52:53 +0000

    changes to scatter plot

--- client/js/cloud_tag.js
+++ client/js/cloud_tag.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
         });
 
 
-        var fill = d3.scale.category20();
+        var fill = d3.scale.category20c();
         var color = d3.scale.linear()
                 //.domain([0,1,2,3,4,5,6,10,15,20,100])
                 //.domain(d3.extent(data, function(d){ return d.word_sum_weight }))
