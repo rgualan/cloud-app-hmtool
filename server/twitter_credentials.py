08465162acef7e56e65c8828ebe08f9fb19809af
Unknown <salgadd@gmail.com>
2017-01-08 08:27:47 +0000

    [Add][server] twitter credential

--- server/twitter_credentials.py
+++ server/twitter_credentials.py
@@ -1,7 +1,7 @@
 # twitter_credentials.py
 # Twitter credential
 # Required for tweet collection
-consumer_key = ""
-consumer_secret = ""
+consumer_key = "RdCtxLXLNfBn06Q5JKAFoe2z5"
+consumer_secret = "A2wJuUQq2rbgDsFWUwln52K2sL70uOrswMolLLeHc05snPiOsy"
 access_token = ""
 access_token_secret = ""
